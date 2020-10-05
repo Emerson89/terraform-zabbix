beee478791669e2d08b70dc8cc971b45a7acd578
Emerson <emerson.cesario50@gmail.com>
2020-10-05 20:20:43 -0300

    add script

---
+++ script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+########################################################################################
+#Autor: Emerson																		   #
+#Titulo: Script de automação para instalação com terraform					           #	
+#Data: 07/12/2019																	   #			
+#																					   #	
+########################################################################################
+clear
+echo 
+echo ========================="Instalando dependencias..."==================================================
+echo
+ sudo yum install epel-release -y && sudo yum install ansible -y &&  sudo yum install git -y &&  sudo yum install unzip -y
+echo
+echo =========================="Fazendo as configurações necessárias..."====================================
+echo
+ cd /tmp
+ git clone git@github.com:Emerson89/zabbix-server.git
+ cd zabbix-server
+echo
+echo =========================="Executando receita ansible..."=============================================
+echo
+sudo ansible-playbook -i hosts -c local zabbix.yml                                                                                                                         
+                                                                                                                                             
+                                     
